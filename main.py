db00307272ed86e7e96b78cde9c4b94c5c2da9ff
Caleb <arias.cm@obmontessori.edu.ph>
2026-02-09 08:22:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+# nested if-statment
+
+from pyscript import display, document
+
+def account_verify(e):
+    document.getElementById('output').innerHTML = '' # resets value
+    
+    username = document.getElementById('input1').value
+
+    password = document.getElementById('input2').value
+
+
+    if len(password) >= 10 and len(username) >= 7:
+        if not password.isalpha():
+            if password.isdigit():
+                display(f"Your username is eligible. Your password must contain both letters and numbers. Please add letters.", target="output")
+    
+            else:
+                display(f"You are signed up, {username}! Welcome!", target="output")
+
+        elif password.isalpha():
+            display(f"Your username is eligible. Your password must contain both letters and numbers. Please add numbers.", target="output")
+
+    elif not len(password) >= 10 and len(username) >= 7:
+            display(f"Your username is eligible. Your password must be 10 characters or more.", target="output")
+    
+    elif not len(password) >= 10 and not len(username) >= 7:
+            display(f"Your username must be 7 characters or more. Your password must be 10 characters or more.", target="output")
+    
+    elif len(password) >= 10 and not len(username) >= 7:
+            display(f"Your username must be 7 characters or more.", target="output")
+
+    
+
+
+
